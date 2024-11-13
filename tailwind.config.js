3a5a532aa207da9fdb9805f4a3bbf9d339b0a2c7
nestorivan <nlopezdev@gmail.com>
2024-11-12 19:30:28 -0700

    add new fonts and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+      fontFamily: {
+        roboto: ["Roboto Mono", "monospace"],
+        oswald: ["Oswald", "sans-serif"],
+        frank: ["Frank Ruhl Libre", "serif"],
+      },
+    },
   },
   plugins: [],
 };
