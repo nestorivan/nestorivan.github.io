61aa213125cb6f808d7407461299894ca173ed65
nestorivan <nlopezdev@gmail.com>
2024-11-21 22:24:22 -0700

    update base url

--- src/components/Home/Header/HeaderPortrait.tsx
+++ src/components/Home/Header/HeaderPortrait.tsx
@@ -2,7 +2,7 @@ const HeroPortrait = () => {
   return (
     <div className="flex flex-col full-w justify-center items-center">
       <img
-        src="/src/assets/nestor.jpeg"
+        src={`${import.meta.env.BASE_URL}/src/assets/nestor.jpeg`}
         alt="Nestor Ivan"
         className="rounded-full w-60 h-60 object-cover mb-4"
       />
